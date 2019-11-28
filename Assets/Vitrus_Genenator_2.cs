6a1560d577765b4372bd2d3e7eddfeb2bb71a502
AlessioBona <alefbona@gmail.com>
2019-11-28 21:38:30 +0100

    small changes

--- Assets/Vitrus_Genenator_2.cs
+++ Assets/Vitrus_Genenator_2.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,39 +15,71 @@ public class Vitrus_Genenator_2 : MonoBehaviour
     public Color[] colors2;
     int actualColor = 0;
 
+    [Header("Game of Live vars")]
+    public int matrixSize = 32;
+    public int cellSize = 4;
+
+    int[,] matrix_1;
+    int[,] matrix_2;
+
     // Start is called before the first frame update
     void Start()
+    {
+        PrepareRenderer();
+        PrepareMatrixes();
+        
+    }
+
+    private void PrepareMatrixes()
+    {
+        // prepare matrix_1 & 2
+    }
+
+    private void PrepareRenderer()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
 
         texture = new Texture2D(textureWidth, textureHeight);
         Rect myRect = new Rect(0, 0, textureWidth, textureHeight);
-        mySprite = Sprite.Create(texture, myRect, new Vector2( .5f,.5f ));
+        mySprite = Sprite.Create(texture, myRect, new Vector2(.5f, .5f));
         spriteRenderer.sprite = mySprite;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // mit timer
+        PlayRound();
+        SetRenderOutput();
+    }
+
+    private void PlayRound()
+    {
+        // GAME OF LIFE
+    }
+
+    private void SetRenderOutput()
+    {
         actualColor++;
-        if(actualColor == colors.Length)
+
+        if (actualColor == colors.Length)
         {
             actualColor = 0;
         }
 
 
-        for (int y = 1; y < texture.height; y = y + 2)
-        {
-            for (int x = 1; x < texture.width; x = x + 2)
-            {
-                Color color = colors[actualColor];
-                texture.SetPixel(x, y, color);
-            }
-        }
+        //for (int y = 1; y < texture.height; y = y + 2)
+        //{
+        //    for (int x = 1; x < texture.width; x = x + 2)
+        //    {
+        //        Color color = colors[actualColor];
+        //        texture.SetPixel(x, y, color);
+        //    }
+        //}
 
-        for (int y = 0; y < texture.height; y = y+2)
+        for (int y = 0; y < texture.height; y = y + 2)
         {
-            for (int x = 0; x < texture.width; x = x+2)
+            for (int x = 0; x < texture.width; x = x + 2)
             {
                 Color color = colors2[actualColor];
                 texture.SetPixel(x, y, color);
